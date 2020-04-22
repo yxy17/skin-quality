91109c4918e449ba80a599109f1b068fd03340ff
Jia Xu <33107182+Uooga@users.noreply.github.com>
2020-04-22 14:28:37 +0800

    Add files via upload

---
+++ public-interface/face-skin-detection/img_preprocess_utils.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = 'Jia Xu'
+
+import cv2
+import numpy as np
+from PIL import Image,ImageOps
+import matplotlib.pyplot as plt
+from skimage import data, img_as_float
+from skimage import exposure
+
+##灰度图处理光照不均
+def unevenLightCompensate_color(img,blockSize):
+    ycrcb = cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
+    channels = cv2.split(ycrcb)
+    gray = channels[0]
+    average = np.mean(gray)
+
+    rows_new = int(np.ceil(gray.shape[0] / blockSize))
+    cols_new = int(np.ceil(gray.shape[1] / blockSize))
+
+    blockImage = np.zeros((rows_new, cols_new), dtype=np.float32)
+    for r in range(rows_new):
+        for c in range(cols_new):
+            rowmin = r * blockSize
+            rowmax = (r + 1) * blockSize
+            if (rowmax > gray.shape[0]):
+                rowmax = gray.shape[0]
+            colmin = c * blockSize
+            colmax = (c + 1) * blockSize
+            if (colmax > gray.shape[1]):
+                colmax = gray.shape[1]
+
+            imageROI = gray[rowmin:rowmax, colmin:colmax]
+            temaver = np.mean(imageROI)
+            blockImage[r, c] = temaver
+
+    blockImage = blockImage - average
+    blockImage2 = cv2.resize(blockImage, (gray.shape[1], gray.shape[0]), interpolation=cv2.INTER_CUBIC)
+    gray2 = gray.astype(np.float32)
+    dst = gray2 - blockImage2
+    dst = dst.astype(np.uint8)
+    dst = cv2.GaussianBlur(dst, (3, 3), 0)
+    dst = cv2.merge(channels,ycrcb)
+    dst = cv2.cvtColor(ycrcb,cv2.COLOR_YCR_CB2BGR,dst)
+    #dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+    return dst
+
+def unevenLightCompensate(img, blockSize):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    average = np.mean(gray)
+
+    rows_new = int(np.ceil(gray.shape[0] / blockSize))
+    cols_new = int(np.ceil(gray.shape[1] / blockSize))
+
+    blockImage = np.zeros((rows_new, cols_new), dtype=np.float32)
+    for r in range(rows_new):
+        for c in range(cols_new):
+            rowmin = r * blockSize
+            rowmax = (r + 1) * blockSize
+            if (rowmax > gray.shape[0]):
+                rowmax = gray.shape[0]
+            colmin = c * blockSize
+            colmax = (c + 1) * blockSize
+            if (colmax > gray.shape[1]):
+                colmax = gray.shape[1]
+
+            imageROI = gray[rowmin:rowmax, colmin:colmax]
+            temaver = np.mean(imageROI)
+            blockImage[r, c] = temaver
+
+    blockImage = blockImage - average
+    blockImage2 = cv2.resize(blockImage, (gray.shape[1], gray.shape[0]), interpolation=cv2.INTER_CUBIC)
+    gray2 = gray.astype(np.float32)
+    dst = gray2 - blockImage2
+    dst = dst.astype(np.uint8)
+    dst = cv2.GaussianBlur(dst, (3, 3), 0)
+    dst = cv2.cvtColor(dst, cv2.COLOR_GRAY2BGR)
+
+    return dst
+
+
+
+def hisEqulColor(img):
+    ycrcb=cv2.cvtColor(img,cv2.COLOR_BGR2YCR_CB)
+    channels=cv2.split(ycrcb)
+    print (len(channels))
+    cv2.equalizeHist(channels[0],channels[0])
+    cv2.merge(channels,ycrcb)
+    cv2.cvtColor(ycrcb,cv2.COLOR_YCR_CB2BGR,img)
+    return img
+
+
+
+def gamma_correction(img=None, gamma=0.5, gain=1.):
+
+    img_out = exposure.adjust_gamma(img, gamma, gain)
+    return img_out
+
+def logri_correction(img=None, gain=1, inv=False):
+
+    img_out = exposure.adjust_log(img, gain, inv)
+    return img_out
+
+
